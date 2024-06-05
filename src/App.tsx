42b8e8ccb8d82470f0207bfdacb1cd32e0629f3d
XNA <Zubbtww77@gmail.com>
2024-06-05 18:13:17 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red-600 p-8">
+      <h1 className="text-2xl font-bold text-gray-800">Hello, Tailwind CSS!</h1>
+      <p className="text-gray-600">Tailwind CSS is awesome!</p>
     </div>
   );
 }
